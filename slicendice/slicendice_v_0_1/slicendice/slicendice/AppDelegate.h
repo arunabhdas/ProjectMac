eaedc890cc238973bd56e7dcc72c728be03359eb
Arunabh Das <arunabhdas@gmail.com>
2013-03-14 17:06:59 -0400

    Initial commit

---
+++ slicendice/slicendice_v_0_1/slicendice/slicendice/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  slicendice
+//
+//  Created by Das Arunabh on 2013-03-14.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ slicendice/slicendice_v_0_1/slicendice/slicendice/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  slicendice
+//
+//  Created by Das Arunabh on 2013-03-14.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
